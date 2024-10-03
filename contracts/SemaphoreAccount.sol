c721d242a78e7fe522dbad94ea716074187045cf
Jimmy Chu <898091+jimmychu0807@users.noreply.github.com>
2024-10-03 11:58:54 +0800

    Upgrade Semaphore to v4.2.0

--- contracts/SemaphoreAccount.sol
+++ contracts/SemaphoreAccount.sol
@@ -8,151 +8,133 @@ import "./semaphore/Semaphore.sol";
 import "./semaphore/interfaces/ISemaphoreVerifier.sol";
 
 contract SemaphoreAccount is BaseAccount, UUPSUpgradeable, Initializable {
-    Semaphore public semaphore;
-    uint256 public groupId;
-    IEntryPoint private immutable _entryPoint;
-    ISemaphoreVerifier verifier;
-    uint256 _currentMerkleRoot;
+  Semaphore public semaphore;
+  uint256 public groupId;
+  IEntryPoint private immutable _entryPoint;
+  ISemaphoreVerifier verifier;
+  uint256 _currentMerkleRoot;
 
-    event SemaphoreAccountInitialized(
-        IEntryPoint indexed entryPoint,
-        uint256 indexed groupId
-    );
+  event SemaphoreAccountInitialized(IEntryPoint indexed entryPoint, uint256 indexed groupId);
 
-    event MerkleRootUpdated(
-        uint256 _currentMerkleRoot,
-        uint256 _latestMerkleRoot
-    );
+  event MerkleRootUpdated(uint256 _currentMerkleRoot, uint256 _latestMerkleRoot);
 
-    modifier verifyGroup() {
-        uint256 latestMerkleRoot = semaphore.getMerkleTreeRoot(groupId);
-        if (_currentMerkleRoot != latestMerkleRoot) {
-            _currentMerkleRoot = latestMerkleRoot;
+  modifier verifyGroup() {
+    uint256 latestMerkleRoot = semaphore.getMerkleTreeRoot(groupId);
+    if (_currentMerkleRoot != latestMerkleRoot) {
+      _currentMerkleRoot = latestMerkleRoot;
 
-            emit MerkleRootUpdated(_currentMerkleRoot, latestMerkleRoot);
-            return;
-        }
-        _;
+      emit MerkleRootUpdated(_currentMerkleRoot, latestMerkleRoot);
+      return;
     }
+    _;
+  }
 
-    constructor(IEntryPoint anEntryPoint) {
-        _entryPoint = anEntryPoint;
-        // _disableInitializers();
+  constructor(IEntryPoint anEntryPoint) {
+    _entryPoint = anEntryPoint;
+    // _disableInitializers();
+  }
+
+  function initialize(
+    address _semaphoreAddress,
+    address _verifierAdress,
+    uint256 _groupId
+  ) public virtual initializer {
+    groupId = _groupId;
+    semaphore = Semaphore(_semaphoreAddress);
+    verifier = ISemaphoreVerifier(_verifierAdress);
+
+    emit SemaphoreAccountInitialized(_entryPoint, _groupId);
+  }
+
+  function entryPoint() public view virtual override returns (IEntryPoint) {
+    return _entryPoint;
+  }
+
+  receive() external payable {}
+
+  function execute(address dest, uint256 value, bytes calldata func) external verifyGroup {
+    _requireFromEntryPoint();
+    _call(dest, value, func);
+  }
+
+  function executeBatch(address[] calldata dest, bytes[] calldata func) external verifyGroup {
+    _requireFromEntryPoint();
+    require(dest.length == func.length, "wrong array lengths");
+    for (uint256 i = 0; i < dest.length; i++) {
+      _call(dest[i], 0, func[i]);
     }
+  }
 
-    function initialize(
-        address _semaphoreAddress,
-        address _verifierAdress,
-        uint256 _groupId
-    ) public virtual initializer {
-        groupId = _groupId;
-        semaphore = Semaphore(_semaphoreAddress);
-        verifier = ISemaphoreVerifier(_verifierAdress);
+  // Validate signature for the UserOperation
+  // ZK Proof of membership and some inputs are encoded in `signature`
+  function _validateSignature(
+    UserOperation calldata userOp,
+    bytes32 userOpHash
+  ) internal virtual override returns (uint256 validationData) {
+    // Decode proof + inputs from signature
+    (
+      uint256[8] memory proof,
+      uint256 merkleTreeRoot,
+      uint256 merkleTreeDepth,
+      uint256 nullifierHash
+    ) = abi.decode(userOp.signature, (uint256[8], uint256, uint256, uint256));
+    uint256 signal = uint256(userOpHash);
 
-        emit SemaphoreAccountInitialized(_entryPoint, _groupId);
+    try
+      verifier.verifyProof(
+        merkleTreeRoot,
+        nullifierHash,
+        signal, // Signal
+        0, // External nullifier
+        proof,
+        merkleTreeDepth
+      )
+    {
+      // Set merkle root used for verification in storage
+      // This will be validated during execution
+      _currentMerkleRoot = merkleTreeRoot;
+
+      return 0; // 0 returned means signature valid as per 4337
+    } catch (bytes memory reason) {
+      return 1;
     }
+  }
 
-    function entryPoint() public view virtual override returns (IEntryPoint) {
-        return _entryPoint;
+  function _call(address target, uint256 value, bytes memory data) internal {
+    (bool success, bytes memory result) = target.call{value: value}(data);
+    if (!success) {
+      assembly {
+        revert(add(result, 32), mload(result))
+      }
     }
+  }
 
-    receive() external payable {}
+  // /**
+  //  * check current account deposit in the entryPoint
+  //  */
+  // function getDeposit() public view returns (uint256) {
+  //     return entryPoint().balanceOf(address(this));
+  // }
 
-    function execute(
-        address dest,
-        uint256 value,
-        bytes calldata func
-    ) external verifyGroup {
-        _requireFromEntryPoint();
-        _call(dest, value, func);
-    }
+  // /**
+  //  * deposit more funds for this account in the entryPoint
+  //  */
+  // function addDeposit() public payable {
+  //     entryPoint().depositTo{value: msg.value}(address(this));
+  // }
 
-    function executeBatch(
-        address[] calldata dest,
-        bytes[] calldata func
-    ) external verifyGroup {
-        _requireFromEntryPoint();
-        require(dest.length == func.length, "wrong array lengths");
-        for (uint256 i = 0; i < dest.length; i++) {
-            _call(dest[i], 0, func[i]);
-        }
-    }
+  // /**
+  //  * withdraw value from the account's deposit
+  //  */
+  // function withdrawDepositTo(
+  //     address payable withdrawAddress,
+  //     uint256 amount
+  // ) public onlyOwner {
+  //     entryPoint().withdrawTo(withdrawAddress, amount);
+  // }
 
-    // Validate signature for the UserOperation
-    // ZK Proof of membership and some inputs are encoded in `signature`
-    function _validateSignature(
-        UserOperation calldata userOp,
-        bytes32 userOpHash
-    ) internal virtual override returns (uint256 validationData) {
-        // Decode proof + inputs from signature
-        (
-            uint256[8] memory proof,
-            uint256 merkleTreeRoot,
-            uint256 merkleTreeDepth,
-            uint256 nullifierHash
-        ) = abi.decode(
-                userOp.signature,
-                (uint256[8], uint256, uint256, uint256)
-            );
-        uint256 signal = uint256(userOpHash);
-
-        try
-            verifier.verifyProof(
-                merkleTreeRoot,
-                nullifierHash,
-                signal, // Signal
-                0, // External nullifier
-                proof,
-                merkleTreeDepth
-            )
-        {
-            // Set merkle root used for verification in storage
-            // This will be validated during execution
-            _currentMerkleRoot = merkleTreeRoot;
-
-            return 0; // 0 returned means signature valid as per 4337
-        } catch (bytes memory reason) {
-            return 1;
-        }
-    }
-
-    function _call(address target, uint256 value, bytes memory data) internal {
-        (bool success, bytes memory result) = target.call{value: value}(data);
-        if (!success) {
-            assembly {
-                revert(add(result, 32), mload(result))
-            }
-        }
-    }
-
-    // /**
-    //  * check current account deposit in the entryPoint
-    //  */
-    // function getDeposit() public view returns (uint256) {
-    //     return entryPoint().balanceOf(address(this));
-    // }
-
-    // /**
-    //  * deposit more funds for this account in the entryPoint
-    //  */
-    // function addDeposit() public payable {
-    //     entryPoint().depositTo{value: msg.value}(address(this));
-    // }
-
-    // /**
-    //  * withdraw value from the account's deposit
-    //  */
-    // function withdrawDepositTo(
-    //     address payable withdrawAddress,
-    //     uint256 amount
-    // ) public onlyOwner {
-    //     entryPoint().withdrawTo(withdrawAddress, amount);
-    // }
-
-    function _authorizeUpgrade(
-        address newImplementation
-    ) internal view override {
-        (newImplementation);
-        // _onlyOwner();
-    }
+  function _authorizeUpgrade(address newImplementation) internal view override {
+    (newImplementation);
+    // _onlyOwner();
+  }
 }
--- contracts/semaphore/interfaces/ISemaphore.sol
+++ contracts/semaphore/interfaces/ISemaphore.sol
@@ -1,127 +1,116 @@
 //SPDX-License-Identifier: MIT
-pragma solidity ^0.8.23;
+pragma solidity 0.8.23;
 
 /// @title Semaphore contract interface.
 interface ISemaphore {
-    error Semaphore__CallerIsNotTheGroupAdmin();
-    error Semaphore__MerkleTreeDepthIsNotSupported();
-    error Semaphore__MerkleTreeRootIsExpired();
-    error Semaphore__MerkleTreeRootIsNotPartOfTheGroup();
-    error Semaphore__YouAreUsingTheSameNillifierTwice();
+  error Semaphore__GroupHasNoMembers();
+  error Semaphore__MerkleTreeDepthIsNotSupported();
+  error Semaphore__MerkleTreeRootIsExpired();
+  error Semaphore__MerkleTreeRootIsNotPartOfTheGroup();
+  error Semaphore__YouAreUsingTheSameNullifierTwice();
+  error Semaphore__InvalidProof();
 
-    /// It defines all the group parameters, in addition to those in the Merkle tree.
-    struct Group {
-        address admin;
-        uint256 merkleTreeDuration;
-        mapping(uint256 => uint256) merkleRootCreationDates;
-        mapping(uint256 => bool) nullifierHashes;
-    }
+  /// It defines all the group parameters used by Semaphore.sol.
+  struct Group {
+    uint256 merkleTreeDuration;
+    mapping(uint256 => uint256) merkleRootCreationDates;
+    mapping(uint256 => bool) nullifiers;
+  }
 
-    /// @dev Emitted when an admin is assigned to a group.
-    /// @param groupId: Id of the group.
-    /// @param oldAdmin: Old admin of the group.
-    /// @param newAdmin: New admin of the group.
-    event GroupAdminUpdated(uint256 indexed groupId, address indexed oldAdmin, address indexed newAdmin);
+  /// It defines all the Semaphore proof parameters used by Semaphore.sol.
+  struct SemaphoreProof {
+    uint256 merkleTreeDepth;
+    uint256 merkleTreeRoot;
+    uint256 nullifier;
+    uint256 message;
+    uint256 scope;
+    uint256[8] points;
+  }
 
-    /// @dev Emitted when the Merkle tree duration of a group is updated.
-    /// @param groupId: Id of the group.
-    /// @param oldMerkleTreeDuration: Old Merkle tree duration of the group.
-    /// @param newMerkleTreeDuration: New Merkle tree duration of the group.
-    event GroupMerkleTreeDurationUpdated(
-        uint256 indexed groupId,
-        uint256 oldMerkleTreeDuration,
-        uint256 newMerkleTreeDuration
-    );
+  /// @dev Event emitted when the Merkle tree duration of a group is updated.
+  /// @param groupId: Id of the group.
+  /// @param oldMerkleTreeDuration: Old Merkle tree duration of the group.
+  /// @param newMerkleTreeDuration: New Merkle tree duration of the group.
+  event GroupMerkleTreeDurationUpdated(
+    uint256 indexed groupId,
+    uint256 oldMerkleTreeDuration,
+    uint256 newMerkleTreeDuration
+  );
 
-    /// @dev Emitted when a Semaphore proof is verified.
-    /// @param groupId: Id of the group.
-    /// @param merkleTreeRoot: Root of the Merkle tree.
-    /// @param nullifierHash: Nullifier hash.
-    /// @param externalNullifier: External nullifier.
-    /// @param signal: Semaphore signal.
-    event ProofVerified(
-        uint256 indexed groupId,
-        uint256 indexed merkleTreeRoot,
-        uint256 nullifierHash,
-        uint256 indexed externalNullifier,
-        uint256 signal
-    );
+  /// @dev Event emitted when a Semaphore proof is validated.
+  /// @param groupId: Id of the group.
+  /// @param merkleTreeDepth: Depth of the Merkle tree.
+  /// @param merkleTreeRoot: Root of the Merkle tree.
+  /// @param nullifier: Nullifier.
+  /// @param message: Semaphore message.
+  /// @param scope: Scope.
+  /// @param points: Zero-knowledge points.
+  event ProofValidated(
+    uint256 indexed groupId,
+    uint256 merkleTreeDepth,
+    uint256 indexed merkleTreeRoot,
+    uint256 nullifier,
+    uint256 message,
+    uint256 indexed scope,
+    uint256[8] points
+  );
 
-    /// @dev Saves the nullifier hash to avoid double signaling and emits an event
-    /// if the zero-knowledge proof is valid.
-    /// @param groupId: Id of the group.
-    /// @param merkleTreeRoot: Root of the Merkle tree.
-    /// @param signal: Semaphore signal.
-    /// @param nullifierHash: Nullifier hash.
-    /// @param externalNullifier: External nullifier.
-    /// @param proof: Zero-knowledge proof.
-    function verifyProof(
-        uint256 groupId,
-        uint256 merkleTreeRoot,
-        uint256 signal,
-        uint256 nullifierHash,
-        uint256 externalNullifier,
-        uint256[8] calldata proof
-    ) external;
+  /// @dev Returns the current value of the group counter.
+  /// @return The current group counter value.
+  function groupCounter() external view returns (uint256);
 
-    /// @dev Creates a new group. Only the admin will be able to add or remove members.
-    /// @param groupId: Id of the group.
-    /// @param depth: Depth of the tree.
-    /// @param admin: Admin of the group.
-    function createGroup(uint256 groupId, uint256 depth, address admin) external;
+  /// @dev See {SemaphoreGroups-_createGroup}.
+  function createGroup() external returns (uint256);
 
-    /// @dev Creates a new group. Only the admin will be able to add or remove members.
-    /// @param groupId: Id of the group.
-    /// @param depth: Depth of the tree.
-    /// @param admin: Admin of the group.
-    /// @param merkleTreeRootDuration: Time before the validity of a root expires.
-    function createGroup(uint256 groupId, uint256 depth, address admin, uint256 merkleTreeRootDuration) external;
+  /// @dev See {SemaphoreGroups-_createGroup}.
+  function createGroup(address admin) external returns (uint256);
 
-    /// @dev Updates the group admin.
-    /// @param groupId: Id of the group.
-    /// @param newAdmin: New admin of the group.
-    function updateGroupAdmin(uint256 groupId, address newAdmin) external;
+  /// @dev It creates a group with a custom Merkle tree duration.
+  /// @param admin: Admin of the group. It can be an Ethereum account or a smart contract.
+  /// @param merkleTreeDuration: Merkle tree duration.
+  /// @return Id of the group.
+  function createGroup(address admin, uint256 merkleTreeDuration) external returns (uint256);
 
-    /// @dev Updates the group Merkle tree duration.
-    /// @param groupId: Id of the group.
-    /// @param newMerkleTreeDuration: New Merkle tree duration.
-    function updateGroupMerkleTreeDuration(uint256 groupId, uint256 newMerkleTreeDuration) external;
+  /// @dev See {SemaphoreGroups-_updateGroupAdmin}.
+  function updateGroupAdmin(uint256 groupId, address newAdmin) external;
 
-    /// @dev Adds a new member to an existing group.
-    /// @param groupId: Id of the group.
-    /// @param identityCommitment: New identity commitment.
-    function addMember(uint256 groupId, uint256 identityCommitment) external;
+  /// @dev See {SemaphoreGroups-_acceptGroupAdmin}.
+  function acceptGroupAdmin(uint256 groupId) external;
 
-    /// @dev Adds new members to an existing group.
-    /// @param groupId: Id of the group.
-    /// @param identityCommitments: New identity commitments.
-    function addMembers(uint256 groupId, uint256[] calldata identityCommitments) external;
+  /// @dev Updates the group Merkle tree duration.
+  /// @param groupId: Id of the group.
+  /// @param newMerkleTreeDuration: New Merkle tree duration.
+  function updateGroupMerkleTreeDuration(uint256 groupId, uint256 newMerkleTreeDuration) external;
 
-    /// @dev Updates an identity commitment of an existing group. A proof of membership is
-    /// needed to check if the node to be updated is part of the tree.
-    /// @param groupId: Id of the group.
-    /// @param identityCommitment: Existing identity commitment to be updated.
-    /// @param newIdentityCommitment: New identity commitment.
-    /// @param proofSiblings: Array of the sibling nodes of the proof of membership.
-    /// @param proofPathIndices: Path of the proof of membership.
-    function updateMember(
-        uint256 groupId,
-        uint256 identityCommitment,
-        uint256 newIdentityCommitment,
-        uint256[] calldata proofSiblings,
-        uint8[] calldata proofPathIndices
-    ) external;
+  /// @dev See {SemaphoreGroups-_addMember}.
+  function addMember(uint256 groupId, uint256 identityCommitment) external;
 
-    /// @dev Removes a member from an existing group. A proof of membership is
-    /// needed to check if the node to be removed is part of the tree.
-    /// @param groupId: Id of the group.
-    /// @param identityCommitment: Identity commitment to be removed.
-    /// @param proofSiblings: Array of the sibling nodes of the proof of membership.
-    /// @param proofPathIndices: Path of the proof of membership.
-    function removeMember(
-        uint256 groupId,
-        uint256 identityCommitment,
-        uint256[] calldata proofSiblings,
-        uint8[] calldata proofPathIndices
-    ) external;
+  /// @dev See {SemaphoreGroups-_addMembers}.
+  function addMembers(uint256 groupId, uint256[] calldata identityCommitments) external;
+
+  /// @dev See {SemaphoreGroups-_updateMember}.
+  function updateMember(
+    uint256 groupId,
+    uint256 oldIdentityCommitment,
+    uint256 newIdentityCommitment,
+    uint256[] calldata merkleProofSiblings
+  ) external;
+
+  /// @dev See {SemaphoreGroups-_removeMember}.
+  function removeMember(
+    uint256 groupId,
+    uint256 identityCommitment,
+    uint256[] calldata merkleProofSiblings
+  ) external;
+
+  /// @dev Saves the nullifier hash to prevent double signaling and emits an event
+  /// if the zero-knowledge proof is valid.
+  /// @param groupId: Id of the group.
+  /// @param proof: Semaphore zero-knowledge proof.
+  function validateProof(uint256 groupId, SemaphoreProof calldata proof) external;
+
+  /// @dev Verifies a zero-knowledge proof by returning true or false.
+  /// @param groupId: Id of the group.
+  /// @param proof: Semaphore zero-knowledge proof.
+  function verifyProof(uint256 groupId, SemaphoreProof calldata proof) external view returns (bool);
 }
