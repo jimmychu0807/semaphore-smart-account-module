3a2869226b4b819495cd256f68474d6502357666
Jimmy Chu <898091+jimmychu0807@users.noreply.github.com>
2024-10-04 19:02:07 +0800

    updated
    Added wasm and zkey with git lfs
    
    Fixing data transform
    
    Updated with non-LFS snark artifacts

--- tasks/deploy.ts
+++ tasks/deploy.ts
@@ -23,9 +23,6 @@ task("deploy:semaphore", "Deploy a Semaphore contract")
         const SemaphoreVerifierFactory = await ethers.getContractFactory("SemaphoreVerifier");
 
         const semaphoreVerifier = await SemaphoreVerifierFactory.deploy();
-
-        console.log("semaphoreVerifier", semaphoreVerifier);
-
         semaphoreVerifierAddress = await semaphoreVerifier.getAddress();
 
         if (logs) {
@@ -67,3 +64,25 @@ task("deploy:semaphore", "Deploy a Semaphore contract")
       };
     }
   );
+
+/**
+ * Defines a Hardhat task to deploy the SemaphoreVerifier contract.
+ * This task can optionally log the deployment address.
+ */
+task("deploy:semaphore-verifier", "Deploy a SemaphoreVerifier contract")
+  .addOptionalParam<boolean>("logs", "Print the logs", true, types.boolean)
+  .setAction(async ({ logs }, { ethers }): Promise<any> => {
+    const SemaphoreVerifierFactory = await ethers.getContractFactory("SemaphoreVerifier");
+
+    const semaphoreVerifier = await SemaphoreVerifierFactory.deploy();
+
+    if (logs) {
+      console.info(
+        `SemaphoreVerifier contract has been deployed to: ${await semaphoreVerifier.getAddress()}`
+      );
+    }
+
+    return {
+      semaphoreVerifier,
+    };
+  });
